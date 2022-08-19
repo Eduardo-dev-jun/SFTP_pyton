ae197e59113e060c4d3a71880b0dfebb31fbdfdb
Eduardo <eduardo.moraes2@gmail.com>
2022-08-19 11:13:47 -0300

    corrigindo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-hostname = 'stgsftp.loggi.com'
-username = 'lola'
-password = 'ewEJNmEESrq86E7J'
-port = 22
+hostname = ''
+username = ''
+password = ''
+port = 2
 allow_agent = False
 look_for_keys = False
