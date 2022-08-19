948b6ae646286d788795812d4076f416a012da97
Eduardo <eduardo.moraes2@gmail.com>
2022-08-19 11:15:11 -0300

    informações de preechimento

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-hostname = ''
-username = ''
-password = ''
-port = 2
+hostname = '' #coloque o host do servidor
+username = '' #coloque o username para conectar com o servidor
+password = '' #coloque a senha para conectar com o servidor
+port = 2 #coloque a porta para conectar com o servidor
 allow_agent = False
 look_for_keys = False
